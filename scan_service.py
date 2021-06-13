fc6a8a2933cfd019dd570550fff4861e2d95e6c4
ndhcoder <noper.dev@gmail.com>
2021-06-13 21:59:59 +0700

    assert result

--- scan_service.py
+++ scan_service.py
@@ -448,7 +448,49 @@ def resolved(link, debug):
     if string_sbd.find("-") > 0 or string_mdt.find("-") > 0:
         success = False
 
-    return { 'success': success, 'student_code': string_sbd, 'permutation_exam_code': string_mdt, 'answers': string_answer_list ,'len': len(all_answer_key)}
+    assert_res = assert_result(string_answer_list)
+    return { 'success': success, 'student_code': string_sbd, 'permutation_exam_code': string_mdt, 'answers': string_answer_list ,'len': len(all_answer_key), 'assert': assert_res}
+
+def assert_result(string_answer_list):
+    result_list = string_answer_list.split('_')
+    my_answers = ["A", "B", "B", "C", "D",
+            "B", "A", "C", "D", "B",
+            "A", "B", "", "C", "A",
+            "D", "C", "A", "A", "A",
+            "C", "C", "C", "B", "C",
+            "D", "C", "D", "D", "D",
+            "A", "A", "A", "A", "D",
+            "D", "B", "B", "B", "B",
+            "D", "A", "B", "C", "D",
+            "A", "B", "C", "D", "B",
+            "C", "B", "B", "A", "B",
+            "C", "B", "B", "C", "D",
+            "A", "A", "B", "B", "C",
+            "D", "C", "B", "C", "D",
+            "A", "A", "B", "B", "B",
+            "B", "C", "C", "D", "D",
+            "A", "B", "B", "C", "D",
+            "A", "B", "B", "B", "C",
+            "A", "B", "C", "D", "A",
+            "", "", "", "", "",
+            "A", "B", "C", "D", "B",
+            "", "", "", "", "",
+            "", "", "", "", "",
+            "", "", "", "", "",
+    ];
+
+    question_size = len(my_answers)
+    result_size = len(result_list)
+    correct_count = 0
+    wa = dict()
+
+    for i in range(0, question_size):
+        if my_answers[i] == result_list[i]:
+            correct_count += 1
+        else:
+            wa[i] =  my_answers[i] + " | " + result_list[i]
+
+    return { 'result': str(correct_count) + '/' + str(question_size), 'wa': wa }
 
 
 def current_milli_time():
